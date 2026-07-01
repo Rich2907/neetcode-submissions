88ec37c83f2dd42759e00f57d6c46e2513a7fbf3
Rich2907 <richa.shaw2907@gmail.com>
2026-07-01 23:40:21 +0530

    Add: single-number - submission-0

---
+++ Data Structures & Algorithms/single-number/submission-0.cpp
@@ -0,0 +1,11 @@
+class Solution {
+public:
+    int singleNumber(vector<int>& nums) {
+        int s=0;
+        for(auto i:nums)
+        {
+                s^=i;
+        }
+        return s;
+    }
+};
